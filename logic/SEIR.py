3fda49db2ffbf2831abe893a4f51014f8069cf76
PGorgolewski <pgorgolewski01@gmail.com>
2022-05-07 23:40:35 +0200

    simulate with new_i >= arrived_infected

--- logic/SEIR.py
+++ logic/SEIR.py
@@ -6,14 +6,20 @@ beta = 0.45
 sigma = 0.1
 gamma = 0.1
 
+
 def simulate(point: Point):
     """Func to simulate point"""
     n = point.N + point.arrived_infected
     s, e, i, r = [val / n for val in (point.S, point.E, point.I + point.arrived_infected, point.R)]
 
-    new_s = point.S - beta*i*s
+    new_s = point.S - beta * i * s
     new_e = point.E + (beta * i * s - sigma * e)
     new_i = sigma * e - gamma * i
     new_r = gamma * i
+
+    if new_i < point.arrived_infected:
+        diff = point.arrived_infected - new_i
+        new_i += diff
+        new_r -= diff
 
     point.S, point.E, point.I, point.R = new_s, new_e, new_i, new_r
