cf501232fb2897939ef3c90ace98f2b8ea234e76
PGorgolewski <pgorgolewski01@gmail.com>
2022-05-07 23:01:21 +0200

    include arrived infected

--- logic/SEIR.py
+++ logic/SEIR.py
@@ -8,7 +8,8 @@ gamma = 0.1
 
 def simulate(point: Point):
     """Func to simulate point"""
-    s, e, i, r = [val / point.N for val in (point.S, point.E, point.I, point.R)]
+    n = point.N + point.arrived_infected
+    s, e, i, r = [val / n for val in (point.S, point.E, point.I + point.arrived_infected, point.R)]
 
     new_s = point.S - beta*i*s
     new_e = point.E + (beta * i * s - sigma * e)
