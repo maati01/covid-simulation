62cdf67b6831560b85b2cb287b760b62955500ad
maati01 <mateuszpow7@gmail.com>
2022-05-10 21:58:09 +0200

    button

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,4 +1,7 @@
+from math import inf
+
 import arcade
+import arcade.gui
 import numpy as np
 from typing import List
 from logic.point import Point
@@ -29,6 +32,20 @@ class GUI(arcade.Window):
         super().__init__(self.y_size, self.x_size + 100,
                          SCREEN_TITLE)  # chwilowo rozmiary mapki wejsciowej, trzeba przeskalowac
 
+        # Creating a UI MANAGER to handle the UI
+        self.uimanager = arcade.gui.UIManager()
+        self.uimanager.enable()
+
+        # Creating Button using UIFlatButton
+        self.start_button = arcade.gui.UIFlatButton(text="Start", width=200)
+
+        # Assigning our on_buttonclick() function
+        self.is_running = False
+        self.start_button.on_click = self.on_button_click
+
+        # Adding button in our uimanager
+        self.uimanager.add(arcade.gui.UIAnchorWidget(anchor_x="left", anchor_y="bottom", child=self.start_button))
+
         self.points = points
         self.all_cords = list(points.keys())
 
@@ -43,7 +60,7 @@ class GUI(arcade.Window):
 
         self._threads_num = threads_num
         self.initialize_grid()
-        #arcade.schedule(self.update_day, 5)
+        # arcade.schedule(self.update_day, 5)
         arcade.schedule(self.simulate, 5)
 
     def update_day(self, delta_time: float) -> None:
@@ -54,10 +71,14 @@ class GUI(arcade.Window):
         self.text = f"Day: {self.day}"
 
     def simulate(self, delta_time: float):
+        if not self.is_running:
+            return
+
         n = len(self.all_cords)
         threads_point_len = round(n / self._threads_num)
-        from_to = [(0 + i*threads_point_len, threads_point_len + i*threads_point_len) for i in range(self._threads_num-1)]
-        from_to.append((0+(self._threads_num-1)*threads_point_len, n))
+        from_to = [(0 + i * threads_point_len, threads_point_len + i * threads_point_len) for i in
+                   range(self._threads_num - 1)]
+        from_to.append((0 + (self._threads_num - 1) * threads_point_len, n))
         threads = [SimulateThread(self.points, self.all_cords, from_to[i]) for i in range(self._threads_num)]
 
         for thread in threads:
@@ -80,19 +101,23 @@ class GUI(arcade.Window):
         """
         Render the screen.
         """
-        # arcade.start_render()
+        arcade.start_render()
 
         # We should always start by clearing the window pixels
-        self.clear()
+        # self.clear()
 
         # Batch draw all the sprites
         self.grid_sprite_list.draw()
+
+        self.uimanager.draw()
+
         arcade.draw_text(self.text, TEXT_PADDING, self.x_size + TEXT_PADDING,
                          arcade.color.BLACK, 40, 80, 'left')
 
         for point in self.points.values():
             if point.I > 0:
                 self.grid_sprites[point.x][point.y].color = arcade.color.GOLD
+
 
     def initialize_grid(self) -> None:
         # Create a list of solid-color sprites to represent each grid location
@@ -108,6 +133,8 @@ class GUI(arcade.Window):
                 self.grid_sprite_list.append(sprite)
                 self.grid_sprites[row].append(sprite)
 
+
+
     def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button.
@@ -121,3 +148,15 @@ class GUI(arcade.Window):
                 self.points[(self.x_size - int(y) + i, int(x) + j)].I = self.points[
                     (self.x_size - int(y) + i, int(x) + j)].N
                 self.points[(self.x_size - int(y) + i, int(x) + j)].S = 0
+
+    def on_button_click(self, event):
+        """
+        This function will be called everytime the user presses the button
+        """
+
+        if self.is_running:
+            self.start_button.text = "Start"
+            self.is_running = False
+        else:
+            self.start_button.text = "Stop"
+            self.is_running = True
