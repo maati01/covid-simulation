e27826ea42c0f7be35cca67fd0d08a4c82c99a7f
PGorgolewski <pgorgolewski01@gmail.com>
2022-05-17 23:30:15 +0200

    fixed seir, stats

--- logic/SEIR.py
+++ logic/SEIR.py
@@ -5,8 +5,9 @@ from math import ceil, floor
 
 # Italy params from link below
 kappa = 0.012
-gamma = 0.017
-beta = 1 #0.22
+gamma = 0.21
+beta = 0.56  # 0.22
+
 
 # http://web.pdx.edu/~gjay/teaching/mth271_2020/html/09_SEIR_model.html
 class GenericModel(ABC):
@@ -18,6 +19,7 @@ class GenericModel(ABC):
         """Abstarct method to simulate point"""
         pass
 
+
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7277829/#:~:text=The%20classical%20SEIR%20model%20can%20be%20described%20by%20a%20series%20of%20ordinary%20differential%20equations%3A
 class SEIR(GenericModel):
     def simulate(self):
@@ -26,18 +28,24 @@ class SEIR(GenericModel):
         s_div_n = self._point.S / n
         e, r = self._point.E, self._point.R
 
-        beta_i_s_div_n, gamma_e, gamma_and_kappa_I = round(beta * self._point.I * s_div_n), round(gamma * e), round((gamma+kappa) * i)
+        beta_i_s_div_n, gamma_e, gamma_and_kappa_I = round(beta * i * s_div_n), round(gamma * e), round(
+            (gamma + kappa) * i)
         new_s, new_e, new_i, new_r = (
             self._point.S - beta_i_s_div_n,
-            self._point.E + beta_i_s_div_n - gamma_e,
-            self._point.I + gamma_e - gamma_and_kappa_I,
-            self._point.R + gamma_and_kappa_I
+            e + beta_i_s_div_n - gamma_e,
+            i + gamma_e - gamma_and_kappa_I,
+            r + gamma_and_kappa_I
         )
 
-        if new_i < self._point.arrived_infected:
-            diff = self._point.arrived_infected - new_i
-            new_i += diff
-            new_r -= diff
+        new_i -= self._point.arrived_infected
+        if new_i < 0:
+            new_r, new_i = new_r + new_i, 0
+
+        if new_r < 0:
+            print("ARRIVED INF: ", self._point.arrived_infected)
+            print(f"s {self._point.S}, i {self._point.I}, r {self._point.R}, e {self._point.E}")
+            print(f"new s {new_s}, new i {new_i}, new_r {new_r}, new e {new_e}")
+            print(f"beta {beta_i_s_div_n}, gamma {gamma_e}, gamma_kappa {gamma_and_kappa_I}")
 
         self._point.S, self._point.E, self._point.I, self._point.R = new_s, new_e, new_i, new_r
         self._point.arrived_infected = 0
@@ -47,9 +55,12 @@ class SEIR(GenericModel):
         people_to_move = round(self._point.N * self._point.move_probability)
         to_neighbours = round(people_to_move * self._point.neighbours_move_probability)
 
+        weights = [val / self._point.N for val in [self._point.S, self._point.E, self._point.I, self._point.R]]
+        states = ['S', 'E', 'I', 'R']
+
         moving_states = choices(
-            ['S', 'E', 'I', 'R'],
-            [val / self._point.N for val in [self._point.S, self._point.E, self._point.I, self._point.R]],
+            [states[i] for i in range(len(states)) if weights[i] > 0],
+            [weight for weight in weights if weight > 0],
             k=people_to_move
         )
 
@@ -66,5 +77,17 @@ class SEIR(GenericModel):
 
             infected_out_neighbours -= ceil(half_of_difference)
             infected_to_neighbours -= floor(half_of_difference)
+
+            if infected_out_neighbours <= 0:
+                infected_to_neighbours += infected_out_neighbours
+                infected_out_neighbours = 0
+                if infected_to_neighbours > 0:
+                    out_of_to_neighbours = round((1 - self._point.neighbours_move_probability) * infected_to_neighbours)
+                    infected_to_neighbours -= out_of_to_neighbours
+                    infected_out_neighbours += out_of_to_neighbours
+                else:
+                    infected_to_neighbours = 0
+            elif infected_to_neighbours < 0:
+                infected_to_neighbours, infected_out_neighbours = infected_out_neighbours, 0
 
         return infected_to_neighbours, infected_out_neighbours
